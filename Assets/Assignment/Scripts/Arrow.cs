01310f3cbee878ffe744ed48534b7b3bab8f5841
simranbaria <simranbaria06@gmail.com>
2024-01-28 14:22:56 -0500

    Bow rotates and spawns an arrow

--- Assets/Assignment/Scripts/Arrow.cs
+++ Assets/Assignment/Scripts/Arrow.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public float speed = 10;
+    public bool shot = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Arrow should be rotated slightly to be aligned on the bow
+        transform.Rotate(0, 0, -45);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if(shot)
+        {
+            
+        }
     }
 }
--- Assets/Assignment/Scripts/Bow.cs
+++ Assets/Assignment/Scripts/Bow.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
+    public float speed = 150;
+    public GameObject arrow;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        drawArrow();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotates bow based on player input
+        float direction = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, direction * speed * Time.deltaTime);
+
+        // Shoot arrow if player presses space
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Shot.");
+        }
+    }
+
+    private void drawArrow()
+    {
+        // Make an arrow appear on the bow
+        Instantiate(arrow, transform.position, transform.rotation);
     }
 }
